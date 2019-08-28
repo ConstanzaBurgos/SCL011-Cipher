d502270aa437fb20b0e1cd10bed86d0becb6b6c8
ConstanzaBurgos <burgos.isabella@gmail.com>
2019-08-28 16:07:55 -0400

    intento subir

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,39 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (offset,string) => {
+    let nuevoTexto = '';
+
+    for (let i = 0; i < string.length; i++) {
+      
+      let convAscii=((string.charCodeAt(i)-65+offset)%26+65);
+       convAscii= String.fromCharCode(convAscii);
+       nuevoTexto+=convAscii;
+
+      
+      }
+         return nuevoTexto;
     
-  },
-  decode: () => {
-    /* Acá va tu código */
-  }
-};
+    }
+  };
+
+  
+  /*window.cipher = {
+    decode: (offset, string) => {
+  
+      let textoDecode = '';
+  
+      for (let i = 0; i < string.length; i++) {
+        
+        let inverAscii=((string.charCodeAt(i)-90-offset)%26+90);
+         inverAscii= String.fromCharCode(inverAscii);
+         textoDecode+=inverAscii;
+  
+        
+        }
+           return textoDecode;
+      
+      }
+    };
+  */
+    
+  
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
-/*Acá va tu código*/
+// Llamo los ID de mis cajas
+
+let botoncitoEncode = document.getElementById("botonCifrado");
+//let botoncitoDecode = document.getElementById("botonDescifrado");
+let claveDescifrada = document.getElementById("claveDescifrada");
+let claveCifrada = document.getElementById("claveCifrada");
+//let botonIngreso= document.getElementById("botonIngreso");
+
+
+
+//llamo bontoncito a cifrar
+
+botoncitoEncode.addEventListener("click",encode);
+function encode() {
+
+	
+let offset = parseInt(document.getElementById("llaveHtml").value);
+
+
+let string = document.getElementById("ingresoNombre").value;
+
+claveCifrada.innerHTML = window.cipher.encode(offset,string);
+
+
+}
+
+//llamo botoncito a descifrar
+
+botoncitoEncode.addEventListener("click",decode);
+function decode() {
+
+	
+let offset = parseInt(document.getElementById("llaveHtml").value);
+
+
+let string = document.getElementById("ingresoNombre").value;
+
+claveDescifrada.innerHTML = window.cipher.decode(offset,string);
+
+
+}
